5ab5521b4968ee05d1da927a5163b9318b0478fe
Ilya Bizyaev <bizyaev.game@yandex.ru>
2015-03-21 13:19:02 +0300

    Now using realloc()
    Thanks to RostakaGmfun.

--- EntangleSink.h
+++ EntangleSink.h
@@ -14,31 +14,19 @@ public:
         Clean();
     }
 
-    /** Most problematic stuff **/
     size_t Put2(const byte *inString, size_t length, int, bool)
     {
         if(!inString || !length)
 			return length;
-        //Reserving new array
-        byte * NewArray = new byte[*out_size+length];
-        memset(NewArray, 0, *out_size+length); //!!!
-        if(*output!=NULL)
-        {
-            //Copying there old data
-            memcpy(NewArray, *output, *out_size);
-            //Deleting old array
-            delete[] *output;
-        }
+        //Reallocating the array
+        *output = (byte*) realloc(*output, *out_size+length);
         //Adding new data
-        byte * WhereToJoin = NewArray + *out_size;
-        memcpy(WhereToJoin, inString, length);
-        //Updating the data address
-        *output = NewArray;
+        byte * WhereToJoin = *output + *out_size;
+        MemCopy(WhereToJoin, inString, length);
         //Updating the size
         *out_size+=length;
         return 0;
     }
-
 
     void Clean()
     {
